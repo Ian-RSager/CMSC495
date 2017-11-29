cd3da09c1b197de445b7f7dcf8219206d97ae3a5
Brian McGillen <brian.mcgillen@gmail.com>
2017-11-28 19:33:27 -0500

    Made connections to character.java and newcharacter.java

--- src/CharacterCreator.java
+++ src/CharacterCreator.java
@@ -3,7 +3,6 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.FontFormatException;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.FocusEvent;
