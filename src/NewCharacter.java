1630e06094b2f08ba42f058c5deb6683e4dd02dd
MaeAnna <33076053+MaeAnna@users.noreply.github.com>
2017-12-14 04:14:02 -0500

    fix for initial subrace

--- src/NewCharacter.java
+++ src/NewCharacter.java
@@ -78,7 +78,6 @@ public class NewCharacter extends JFrame{
     private Character myCharacter;
     private int hitdice = 0;   //Character's hit dice
     
-//    private  MasterLists ml;
     private ArrayList<String> selectedFeatList = null;
     
     //Constructor
@@ -131,7 +130,6 @@ public class NewCharacter extends JFrame{
         });
 
         //Create a new character to populate
-        //ml = new MasterLists();
         myCharacter = new Character();
 
         //Dont allow user to close it
@@ -150,7 +148,7 @@ public class NewCharacter extends JFrame{
     
     //Main Test Function
     //Will be taken out when linked with the rest of the program
-    public static void main(String[] args){
+ //   public static void main(String[] args){
         //JFrame frame1 = new JFrame("DialogEx");
         //frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //frame1.setSize (700, 685);
@@ -158,10 +156,10 @@ public class NewCharacter extends JFrame{
         //frame1.setLocationRelativeTo(null);
         //frame1.setVisible(true);
       
-        NewCharacter nc = new NewCharacter();
-        nc.newCharacterGui();
+ //       NewCharacter nc = new NewCharacter();
+ //       nc.newCharacterGui();
         
-    }
+//    }
 
 
 
@@ -195,7 +193,7 @@ public class NewCharacter extends JFrame{
         private String [] racesStr      = {"Dwarf","Elf","Human","Halfling",
                                            "Dragonborn","Gnome","Half-Elf",
                                            "Half-Orc","Tiefling"};
-        private String [] subracesStr   = {"Hill Dwarf","Mountain Dwarf"};
+        private String [] subracesStr   = {"None","Hill Dwarf","Mountain Dwarf"};
         private JComboBox<String> raceComboBox  = new JComboBox<String> (racesStr);
         private JComboBox<String> subRaceComboBox = new JComboBox<String> (subracesStr);
 
@@ -376,6 +374,7 @@ public class NewCharacter extends JFrame{
                     String rStr = selected.toString();
                     
                     raceLabel.setText(rStr);
+                    subRaceComboBox.addItem("None");
                     switch(rStr){
                         case "Dwarf":
                             updateRaceImg("Dwarf.jpg",iicon);
@@ -698,7 +697,7 @@ public class NewCharacter extends JFrame{
             left.add(helper);
     
    // Remove feature selection         
-  //          left.add(addFeatures);
+            left.add(addFeatures);
             
             //Add Left Panel to Gridbag Layout
             gbc.fill = GridBagConstraints.NONE;
@@ -1561,7 +1560,7 @@ public class NewCharacter extends JFrame{
             iFlow.setOpaque(false);
             JButton imgPrevButton = new JButton();
             try{
-                Image prevImg = ImageIO.read(getClass().getResource("Back24.gif"));
+                Image prevImg = ImageIO.read(new File("Back24.gif"));
                 imgPrevButton.setIcon(new ImageIcon(prevImg));
             }
             catch(Exception ee){
@@ -1572,7 +1571,7 @@ public class NewCharacter extends JFrame{
  
             JButton imgNextButton = new JButton();
             try{
-                Image nextImg = ImageIO.read(getClass().getResource("Forward24.gif"));
+                Image nextImg = ImageIO.read(new File("Forward24.gif"));
                 imgNextButton.setIcon(new ImageIcon(nextImg));
             }
             catch(Exception ee){
